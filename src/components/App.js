c9f327bce7fed5ea7ca28d39796674940aed5e12
useashu <75379606+useashu@users.noreply.github.com>
2022-04-06 23:27:28 +0530

    updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,20 +8,20 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const apikey=process.env.API_KEY;
+  //const apikey=process.env.API_KEY;
   return (
     <BrowserRouter>
     <div className="App">
       
         <Navbar/>
         <Routes>
-        <Route exact path="/business" element={<News apikey={apikey} key="business" pageSize={6} country="in" category="business"/>} />
-        <Route exact path="/sports" element={<News apikey={apikey} key="sports" pageSize={6} country="in" category="sports"/>} />
-        <Route exact path="/science" element={<News apikey={apikey} key="science" pageSize={6} country="in" category="science"/>} />
-        <Route exact path="/entertainment" element={<News apikey={apikey} key="entertainment" pageSize={6} country="in" category="entertainment"/>} />
-        <Route exact path="/" element={<News apikey={apikey} key="general" pageSize={6} country="in" category="general"/>} />
-        <Route exact path="/health" element={<News apikey={apikey}  key="health" pageSize={6} country="in" category="health"/>} />
-        <Route exact path="/technology" element={<News apikey={apikey} key="technology" pageSize={6} country="in" category="technology"/>} />
+        <Route exact path="/business" element={<News  key="business" pageSize={6} country="in" category="business"/>} />
+        <Route exact path="/sports" element={<News  key="sports" pageSize={6} country="in" category="sports"/>} />
+        <Route exact path="/science" element={<News  key="science" pageSize={6} country="in" category="science"/>} />
+        <Route exact path="/entertainment" element={<News  key="entertainment" pageSize={6} country="in" category="entertainment"/>} />
+        <Route exact path="/" element={<News  key="general" pageSize={6} country="in" category="general"/>} />
+        <Route exact path="/health" element={<News   key="health" pageSize={6} country="in" category="health"/>} />
+        <Route exact path="/technology" element={<News key="technology" pageSize={6} country="in" category="technology"/>} />
         </Routes>
      
     </div>
